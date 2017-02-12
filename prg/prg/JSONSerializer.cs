14b4130c77fa375de59c9b84bb81b102c8ad1ba2
Sergey Mormul <smorkva@gmail.com>
2017-02-12 13:36:09 +0200

    Simple code

--- prg/prg/JSONSerializer.cs
+++ prg/prg/JSONSerializer.cs
@@ -1,11 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections;
 
 namespace prg
 {
     class JSONSerializer
     {
+        public string parseType(object obj, int depth) {
+            if (obj == null)
+                return "";
+            string res = "";
+            string prefix = "".PadLeft(depth, '\t');
+            string fields = "";
+
+            var currType = obj.GetType();
+
+            if (currType == typeof(int) ||
+                currType == typeof(string) ||
+                currType == typeof(bool) ||
+                currType == typeof(float) ||
+                currType == typeof(double) ||
+                currType == typeof(UInt32) ||
+                currType == typeof(UInt64) ||
+                currType == typeof(System.DateTime))
+            {
+                res += prefix + currType.Name + ": \"" + obj + "\",\r\n";
+            }
+            else if (currType.IsEnum)
+            {
+                Array enumValues = System.Enum.GetValues(currType);
+                res += prefix + currType.Name + ": [";
+                for (var i = 0; i < enumValues.Length; i++)
+                {
+                    res += enumValues.GetValue(i).ToString();
+                    if (i < enumValues.Length - 1)
+                        res += ',';
+                }
+                res += "],\r\n";
+
+            }
+            else if (currType.IsGenericParameter) {
+              
+            }
+            else if (currType.IsGenericType) {
+                if (obj is IEnumerable)
+                {
+                    foreach (object o in (obj as IEnumerable))
+                    {
+                        fields += this.parseType(o, depth + 1);
+                    }
+                    res += prefix + currType.Name + ": {\r\n" + fields + prefix + "},\r\n";
+                }
+                else {
+                    var key = currType.GetProperty("Key")
+                        .GetValue(obj, null);
+                    object o = currType.GetProperty("Value")
+                        .GetValue(obj, null);
+                    res += prefix + key + ": {\r\n" + this.parseType(o, depth + 1) + prefix + "},\r\n";
+                }
+                
+            }
+            else if (currType == typeof(ArrayList))
+            {
+                var aList = obj as ArrayList;
+
+                foreach (var item in aList)
+                {
+                    fields += this.parseType(item, depth + 1);
+                }
+                res += prefix + currType.Name + ": {\r\n" + fields + prefix + "},\r\n";
+            }
+            else
+            {
+                foreach (var field in currType.GetFields())
+                {
+                    fields += this.parseType(field.GetValue(obj), depth + 1);
+                }
+                res += prefix + currType.Name + ": {\r\n" + fields + prefix + "},\r\n";
+            }
+            
+            
+            return res;
+        }
+        public string serialize(object obj) {
+            if (obj == null) {
+                return "{}";
+            }
+            var type = obj.GetType();
+
+            string fields = this.parseType(obj, 1);
+            return "{" + string.Format("\r\n\tname:{0}, \r\n{1}", type.Name, fields) + "}";
+        }
     }
 }
--- prg/prg/Program.cs
+++ prg/prg/Program.cs
@@ -1,14 +1,57 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
+using System.Web.Script.Serialization;
 
 namespace prg
 {
+    public enum someEnum {
+        one,
+        two,
+        tree
+    }
+    struct infoItem {
+        public string name;
+        public someEnum value;
+        public infoItem(string name, someEnum value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+    }
+    [Serializable]
+    struct someData {
+        public string firstField;
+        public int intValue;
+        public Dictionary<int, infoItem> someCollect;
+        public ArrayList arrayList;
+        public List<string> list;
+        public string _data { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var serializer = new JSONSerializer();
+            var target = new someData();
+            
+            target.firstField = "I am first";
+            target.intValue = 2;
+            target.someCollect = new Dictionary<int, infoItem>();
+                target.someCollect.Add(4, new infoItem("one and tree", someEnum.one | someEnum.tree));
+                target.someCollect.Add(3, new infoItem("one and two", someEnum.one | someEnum.two));
+                target.someCollect.Add(5, new infoItem("two and tree", someEnum.two | someEnum.two));
+            target.list = new List<string>();
+                target.list.Add("First item in list");
+            target.arrayList = new ArrayList();
+                target.arrayList.Add(123);
+                target.arrayList.Add("some string");
+                target.arrayList.Add(new infoItem("one two tree",
+                        someEnum.one | someEnum.two | someEnum.two));
+
+            Console.WriteLine(serializer.serialize(target));
+            Console.ReadLine();
         }
     }
 }
